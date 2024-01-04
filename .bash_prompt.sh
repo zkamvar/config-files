ab45a767dfcec99924cc99a815bdfa1453e8f0c9
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-04 11:33:11 -0800

    more prompt engineering

--- .bash_prompt.sh
+++ .bash_prompt.sh
@@ -1,7 +1,6 @@
 function color_my_prompt { 
     local __user_and_host="\[\033[01;32m\]\u@\h"
     local __cur_location="\[\033[01m\]\t \w"
-    local __git_branch='`git symbolic-ref HEAD --short 2> /dev/null | sed -E  s/^\(.+\)$/\(\\\\\1\)\/`'
     local __prompt_tail="$"
     local __last_color="\[\033[00m\]"
 
@@ -10,16 +9,18 @@ function color_my_prompt {
     local LPURPLE="\[\033[0;35m\]"
     local GREEN="\[\033[0;32m\]"
     local LBLUE="\[\033[0;34m\]"
-    local state
 
     local git_status
     local git_stash
+    local git_branch
+    git_branch=$(git symbolic-ref HEAD --short 2> /dev/null)
     # Capture the output of the "git status" command.
     git_status="$(git status 2> /dev/null)"
     # Counting how many stashes exist
     git_stash="$(git stash list 2> /dev/null | wc -l)"
 
     # Set color based on clean/staged/dirty.
+    local state
     if [[ ${git_status} =~ "othing to commit" ]]; then
 	      state="${LBLUE}"
     elif [[ ${git_status} =~ "Changes to be committed" ]]; then
@@ -29,7 +30,8 @@ function color_my_prompt {
     fi
 
     # collect the number of commits ahead or behind origin
-    local sym="$(echo \"${git_status}\" | sed -E 's/.*by[[:blank:]]([0-9]*)[[:blank:]]commit.*/\1/' | grep '[0-9]' 2> /dev/null)"
+    local sym
+    sym=$(echo "${git_status}" | sed -E 's/.*by[[:blank:]]([0-9]*)[[:blank:]]commit.*/\1/' | grep '[0-9]' 2> /dev/null)
 
     # add marker for origin status with number of commits ahead (+) or behind (-)
     if [[ ${git_status} =~ "branch is ahead of" ]]; then
@@ -37,17 +39,18 @@ function color_my_prompt {
     elif [[ ${git_status} =~ "branch is behind" ]]; then
         sym="[-${sym}]"
     elif [[ ${git_status} =~ "HEAD detached at" ]]; then
-        __git_branch="$(echo ${git_status} | sed -E 's/HEAD detached at ([^ ]+).*$/(🙃HEAD @ \1)/' 2> /dev/null)"
+        git_branch=$(echo "${git_status}" | sed -E 's/HEAD detached at ([^ ]+).*$/(🙃HEAD @ \1)/' 2> /dev/null)
         sym=""
     else
         sym=""
     fi
+    # add marker for the state of the git stash
     local stash=""
     if [[ ! ${git_stash} -eq 0 ]]; then
         stash="{📦${git_stash}}"
     fi
 
-    export PS1="$__cur_location\n${state}$__git_branch${sym}${stash}$__last_color$__prompt_tail "
+    export PS1="${__cur_location}\n${state}(${git_branch})${sym}${stash}${__last_color}${__prompt_tail} "
 }
 # Tell bash to execute this function just before displaying its prompt.
 PROMPT_COMMAND=color_my_prompt
