7c7ea08376e8a4ab037e9637a890a14d7c203bca
Zhian N. Kamvar <zkamvar@gmail.com>
2025-06-21 14:34:38 -0700

    add rust treesitter

--- nvim/lua/plug/treesitter.lua
+++ nvim/lua/plug/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "r", "rnoweb", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+  ensure_installed = { "rust", "toml", "r", "rnoweb", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
