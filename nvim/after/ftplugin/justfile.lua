eefffcd78491b9bddf03b48a7681e414c205e4c0
Zhian Kamvar <zhian.kamvar@getrecast.com>
2025-07-30 07:36:22 -0700

    attempt to get justfiles to behave

---
+++ nvim/after/ftplugin/justfile.lua
@@ -0,0 +1,6 @@
+local o = vim.o
+
+o.expandtab = false 
+o.smartindent = true # syntax aware indentations for newline inserts
+o.tabstop = 2 # num of space characters per tab
+o.shiftwidth = 2 # spaces per indentation level
