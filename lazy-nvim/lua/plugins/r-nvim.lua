858e4f70cf10def82376da6baec1a8530452739c
Zhian Kamvar <zhian.kamvar@getrecast.com>
2025-08-14 07:27:15 -0700

    FIX MY R EDITOR PROBLEM
    It turns out that r-nvim does not like fractional widths, so the
    solution was to floor it.

--- lazy-nvim/lua/plugins/r-nvim.lua
+++ lazy-nvim/lua/plugins/r-nvim.lua
@@ -25,7 +25,7 @@ opts.min_editor_width = 80
 -- set the working directory to be vim's working directory
 setwd = "nvim"
 -- control the size of the R console
-opts.rconsole_width = vim.fn.winwidth(0) / 2
+opts.rconsole_width = math.floor(vim.fn.winwidth(0) / 2)
 opts.auto_quit = true
 -- opts.external_term = "kitty_split"
 opts.rconsole_height = 10
