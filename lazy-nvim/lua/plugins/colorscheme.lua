3a85b7e4ab84a465c205760ca18b4b1a7d3f02ec
Zhian N. Kamvar <zkamvar@gmail.com>
2025-08-16 08:10:31 -0700

    use noctis themes

--- lazy-nvim/lua/plugins/colorscheme.lua
+++ lazy-nvim/lua/plugins/colorscheme.lua
@@ -9,11 +9,11 @@ return {
     opts = {
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("colorscheme nightfox")
+        vim.cmd("colorscheme noctis_viola")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd("colorscheme dayfox")
+        vim.cmd("colorscheme noctis_hibernus")
       end,
       update_interval = 3000,
       fallback = "light",
