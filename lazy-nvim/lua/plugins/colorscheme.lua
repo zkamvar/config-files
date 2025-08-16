e059fa226045855a82e46dc60f4c00daeed3df92
Zhian N. Kamvar <zkamvar@gmail.com>
2025-08-16 07:43:54 -0700

    add auto dark mode so I can easily switch themes
    See https://github.com/LazyVim/LazyVim/issues/279#issuecomment-1546217018

--- lazy-nvim/lua/plugins/colorscheme.lua
+++ lazy-nvim/lua/plugins/colorscheme.lua
@@ -5,17 +5,24 @@ return {
   { "rose-pine/neovim", name = "rose-pine" },
   { "talha-akram/noctis.nvim" },
   {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme nightfox")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme dayfox")
+      end,
+      update_interval = 3000,
+      fallback = "light",
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
-      -- colorscheme = function()
-      --   local is_dark = vim.o.background == "dark"
-      --   if is_dark then
-      --     require("nightfox").load()
-      --   else
-      --     require("dayfox").load()
-      --   end
-      -- end,
     },
   },
 }
