a6c045cee3cc2213bf3789f2ceac5f1b40ea9181
Zhian N. Kamvar <zkamvar@gmail.com>
2025-08-23 16:46:58 -0700

    fiddle with color schemes more

--- lazy-nvim/lua/plugins/colorscheme.lua
+++ lazy-nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,41 @@ return {
   { "EdenEast/nightfox.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "talha-akram/noctis.nvim" },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "storm",
+      day_brightness = 0.4,
+    },
+  },
+  {
+    -- TODO: This prevents an issue where catppuccin updated their method for
+    -- bufferline and it broke LazyVim. The author of LazyVim is on vacation,
+    -- so this is a workaround until they get back.
+    -- <https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3203793593>
+    "catppuccin",
+    optional = true,
+    opts = function()
+      local bufferline = require("catppuccin.groups.integrations.bufferline")
+      bufferline.get = bufferline.get or bufferline.get_theme
+    end,
+  },
   -- {
   --   "f-person/auto-dark-mode.nvim",
   --   opts = {
@@ -13,7 +48,7 @@ return {
   --     end,
   --     set_light_mode = function()
   --       vim.api.nvim_set_option_value("background", "light", {})
-  --       vim.cmd("colorscheme catppuccin-latte")
+  --       vim.cmd("colorscheme catppuccin")
   --     end,
   --     update_interval = 3000,
   --     fallback = "light",
@@ -22,7 +57,36 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = function()
+        local colorschemes = {
+          "zenbones",
+          "rosebones",
+          "tokyobones",
+          "zenwritten",
+          "neobones",
+          -- "tokyonight",
+          -- "catppuccin",
+        }
+        -- stolen from randombones
+        if vim.g.colors_name then
+          vim.api.nvim_command([[highlight clear]])
+        end
+
+        local util = require("zenbones.util")
+
+        math.randomseed(os.time())
+        local index = math.random(#colorschemes)
+        local colorscheme = colorschemes[index]
+        vim.notify(
+          "colorscheme: " .. colorscheme,
+          "info",
+          { title = "plugins/colorscheme.lua" }
+        )
+
+        vim.g.colors_name = colorscheme
+
+        util.apply_colorscheme()
+      end,
     },
   },
 }
