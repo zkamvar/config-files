0ffdb788facd6df13d95fe26367ae8de0e517b08
Zhian Kamvar <zhian.kamvar@getrecast.com>
2025-08-22 17:27:22 -0700

    oh my something happened

--- lazy-nvim/lua/plugins/colorscheme.lua
+++ lazy-nvim/lua/plugins/colorscheme.lua
@@ -4,25 +4,25 @@ return {
   { "EdenEast/nightfox.nvim" },
   { "rose-pine/neovim", name = "rose-pine" },
   { "talha-akram/noctis.nvim" },
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("colorscheme rose-pine-moon")
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd("colorscheme catppuccin-latte")
-      end,
-      update_interval = 3000,
-      fallback = "light",
-    },
-  },
+  -- {
+  --   "f-person/auto-dark-mode.nvim",
+  --   opts = {
+  --     set_dark_mode = function()
+  --       vim.api.nvim_set_option_value("background", "dark", {})
+  --       vim.cmd("colorscheme rose-pine-moon")
+  --     end,
+  --     set_light_mode = function()
+  --       vim.api.nvim_set_option_value("background", "light", {})
+  --       vim.cmd("colorscheme catppuccin-latte")
+  --     end,
+  --     update_interval = 3000,
+  --     fallback = "light",
+  --   },
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "tokyonight",
     },
   },
 }
