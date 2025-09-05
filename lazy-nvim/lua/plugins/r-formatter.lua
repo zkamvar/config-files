d2303fc071f405562983c46d86acab8aa0b5038b
Zhian Kamvar <zhian.kamvar@getrecast.com>
2025-09-05 06:51:31 -0700

    make the air formatter more flexible

--- lazy-nvim/lua/plugins/r-formatter.lua
+++ lazy-nvim/lua/plugins/r-formatter.lua
@@ -1,8 +1,17 @@
 return {
   require("lspconfig").air.setup({
     on_attach = function(_, bufnr)
+      -- Only run air format on projects that use air formatting.
+      -- NOTE: I could probably run a detector to see if an air.toml exists
+      local fixable_dirs = {
+        "getrecast/recast",
+        "poppr",
+      }
       local current_dir = vim.fn.expand("%:p:h", true)
-      local okay_to_fix = current_dir:match("getrecast/recast/")
+      local okay_to_fix = false
+      for dir in fixable_dirs do
+        okay_to_fix = current_dir:match(dir) or okay_to_fix
+      end
       if okay_to_fix then
         vim.api.nvim_create_autocmd("BufWritePre", {
           buffer = bufnr,
