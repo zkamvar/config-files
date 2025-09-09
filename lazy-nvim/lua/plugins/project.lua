bf1eb943b48138a5efe1847951688b5131763a1b
Zhian N. Kamvar <zkamvar@gmail.com>
2025-09-08 17:35:47 -0700

    try to get project to recognize DESCRIPTION files

---
+++ lazy-nvim/lua/plugins/project.lua
@@ -0,0 +1,32 @@
+return {
+  "ahmedkhalf/project.nvim",
+  opts = {
+    manual_mode = true,
+    patterns = {
+      "DESCRIPTION",
+      ".git",
+      "_darcs",
+      ".hg",
+      ".bzr",
+      ".svn",
+      "Makefile",
+      "package.json",
+    },
+  },
+  event = "VeryLazy",
+  config = function(_, opts)
+    require("project_nvim").setup(opts)
+    local history = require("project_nvim.utils.history")
+    history.delete_project = function(project)
+      for k, v in pairs(history.recent_projects) do
+        if v == project.value then
+          history.recent_projects[k] = nil
+          return
+        end
+      end
+    end
+    LazyVim.on_load("telescope.nvim", function()
+      require("telescope").load_extension("projects")
+    end)
+  end,
+}
