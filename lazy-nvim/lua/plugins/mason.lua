07d4140d139173f71597721100d21d66cefe1d32
Zhian N. Kamvar <zkamvar@gmail.com>
2025-09-13 07:44:54 -0700

    use mason
    but I am not sure this is the right way to go about it

---
+++ lazy-nvim/lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "air",
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+}
