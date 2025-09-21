15a9612a5918ec3ca3bfdf26d27a0e16ac5dfda6
Zhian N. Kamvar <zkamvar@gmail.com>
2025-09-21 08:38:41 -0700

    add meow.yarn

--- lazy-nvim/lua/config/keymaps.lua
+++ lazy-nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>yt", function()
+  require("meow.yarn").open_tree("type_hierarchy", "supertypes")
+end, { desc = "Yarn: Type Hierarchy (Super)" })
+vim.keymap.set("n", "<leader>yT", function()
+  require("meow.yarn").open_tree("type_hierarchy", "subtypes")
+end, { desc = "Yarn: Type Hierarchy (Sub)" })
+vim.keymap.set("n", "<leader>yc", function()
+  require("meow.yarn").open_tree("call_hierarchy", "callers")
+end, { desc = "Yarn: Call Hierarchy (Callers)" })
+vim.keymap.set("n", "<leader>yC", function()
+  require("meow.yarn").open_tree("call_hierarchy", "callees")
+end, { desc = "Yarn: Call Hierarchy (Callees)" })
---
+++ lazy-nvim/lua/plugins/meow.yarn.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "retran/meow.yarn.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = function()
+      require("meow.yarn").setup({
+        -- Your custom configuration goes here
+      })
+    end,
+  },
+}
