23073b7eb9291b0e3928f70c2b800894f7d33220
Zhian Kamvar <zhian.kamvar@getrecast.com>
2026-03-11 07:44:50 -0700

    use latest r-nvim release

--- lazy-nvim/lua/plugins/r-nvim.lua
+++ lazy-nvim/lua/plugins/r-nvim.lua
@@ -169,6 +169,7 @@ return {
   "R-nvim/R.nvim",
   -- Only required if you also set defaults.lazy = true
   lazy = false,
+  version = "*",
   -- R.nvim is still young and we may make some breaking changes from time
   -- to time (but also bug fixes all the time). If configuration stability
   -- is a high priority for you, pin to the latest minor version, but unpin
